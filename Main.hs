fcc75c5a9e6a1b8c2a50d8c9c7d14b315a824db8
Sergey Batin <the.batin@gmail.com>
2017-06-30 22:49:22 +0300

    font builder refactored

--- Main.hs
+++ Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
 import Codec.BMP
@@ -8,16 +10,17 @@ import Data.List
 import Data.Word
 import Font
 import System.FilePath
+import System.IO
 import Text.Printf
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Unsafe as BSU
 
-type Font = BS.ByteString
+type FontData = BS.ByteString
 
-stringify :: Font -> String
+stringify :: FontData -> String
 stringify = intercalate "," . map (printf "0x%02X") . BS.unpack
 
-renderSymbol :: Char -> Font -> IO ()
+renderSymbol :: Char -> FontData -> IO ()
 renderSymbol ch font = do
     putStrLn line
     forM_ [0..pred h] $ \y -> do
@@ -59,9 +62,8 @@ getPixel (x, y) ((w, h), rgba) = (r, g, b) where
 hasData :: Pixel -> Bool
 hasData (r, g, b) = r < 255 || g < 255 || b < 255
 
-takeCellData :: Int -> RGBA -> Cell
-takeCellData cellIndex rgba = map (\y -> map (\x -> hasData $ getPixel (x, y) rgba) [x1..x2]) [y1..y2] where
-    cellSize = (42, 43)
+takeCellData :: Int -> (Int, Int) -> RGBA -> Cell
+takeCellData cellIndex cellSize rgba = map (\y -> map (\x -> hasData $ getPixel (x, y) rgba) [x1..x2]) [y1..y2] where
     headerHeight = 17
 
     cellsInRow = fst (fst rgba) `div` (fst cellSize - 1)
@@ -73,14 +75,7 @@ takeCellData cellIndex rgba = map (\y -> map (\x -> hasData $ getPixel (x, y) rg
     x2 = x1 + fst cellSize - 1
     y2 = y1 + snd cellSize - 1
 
-takeFontData :: Cell -> Cell
-takeFontData = slice (snd fontSize) (snd fontOffset) . map (slice (fst fontSize) (fst fontOffset)) where
-    slice n m = take n . drop m
-
-    fontSize = (20, 28)
-    fontOffset = (10, 7)
-
-packFontData :: Cell -> Font
+packFontData :: Cell -> FontData
 packFontData = BS.pack . loop . concat where
     loop [] = []
     loop xs = byte : loop zs where
@@ -95,27 +90,80 @@ drawCellData cell = unlines (line : loop cell ++ [line]) where
     loop (xs:xss) = row : loop xss where
         row = '|' : map (\b -> if b then '#' else ' ') xs ++ "|"
 
-renderSymbol2 :: Char -> RGBA -> IO ()
-renderSymbol2 ch rgba = putStrLn $ drawCellData $ takeFontData $ takeCellData (ord ch - 32) rgba
+data Options = Options {
+    filePath :: FilePath,
+    cellSize :: (Int, Int),
+    fontSize :: (Int, Int),
+    fontOffset :: (Int, Int)
+}
 
-makeFontFile :: RGBA -> FilePath -> IO Font
-makeFontFile rgba filePath = do
+data Font = Font {
+    fontData :: FontData,
+    fontNumChars :: Int
+}
+
+loadFont :: Options -> IO Font
+loadFont Options{..} = do
+    rgba <- loadData filePath
+
     let
-        header = BS.pack [20, 28, 32, 95]
-        symbols = map (\ch -> packFontData $ takeFontData $ takeCellData (ord ch - 32) rgba) [' '..'Z']
-        size = BS.length header + sum (map BS.length symbols)
+        start = ' '
+        chars = [start..'Z']
+        header = BS.pack $ map fromIntegral [fst fontSize, snd fontSize, ord start, length chars]
+        charsData = map (\ch -> toFontData $ takeCellData (ord ch - ord start) cellSize rgba) chars
 
-        content = printf "uint8_t Ubuntu[%d] PROGMEM={" size : map stringify (header:symbols) ++ ["};"]
+    return Font {
+        fontData = BS.append header $ BS.concat charsData,
+        fontNumChars = length chars
+    }
+    where
+        slice n m = take n . drop m
 
-    --putStrLn $ stringify (symbols !! 16)
-    writeFile filePath $ intercalate ",\n" content
-    return $ BS.append header $ BS.concat symbols
+        toFontData = packFontData . slice (snd fontSize) (snd fontOffset) . map (slice (fst fontSize) (fst fontOffset))
+
+saveFont :: String -> Font -> IO ()
+saveFont fontName Font{..} = do
+    h <- openFile ("ili9481/" ++ fontName ++ ".c") WriteMode
+    hPutStrLn h "#include <avr/pgmspace.h>"
+    hPutStrLn h $ printf "const uint8_t %s[%d] PROGMEM={" fontName (BS.length fontData)
+    hPutStrLn h $ stringify header ++ ","
+
+    let loop n xs = unless (BS.null xs) $ do
+            let (content, xs') = BS.splitAt charDataSize xs
+            hPutStrLn h $ stringify content ++ ", // " ++ [chr n]
+            loop (succ n) xs'
+
+    loop (fromIntegral $ BSU.unsafeIndex header 2) charsData
+    hPutStrLn h "};"
+    hClose h
+    where
+        (header, charsData) = BS.splitAt 4 fontData
+        charDataSize = BS.length charsData `div` fontNumChars
+
+--renderSymbol2 :: Char -> RGBA -> IO ()
+--renderSymbol2 ch rgba = putStrLn $ drawCellData $ takeFontData $ takeCellData (ord ch - 32) rgba
+
+font1 = Options {
+    filePath = "/Users/sergey/Desktop/test-2.bmp",
+    cellSize = (34, 35),
+    fontSize = (16, 24),
+    fontOffset = (8, 5)
+}
+
+font2 = Options {
+    filePath = "/Users/sergey/Desktop/test.bmp",
+    cellSize = (42, 43),
+    fontSize = (20, 28),
+    fontOffset = (10, 7)
+}
 
 main = do
-    rgba <- loadData "/Users/sergey/Desktop/test.bmp"
-    renderSymbol2 '0' rgba
-    renderSymbol2 '4' rgba
-    renderSymbol 'W' fontdatatype
+    font@Font{..} <- loadFont font1
+    --renderSymbol2 '0' rgba
+    --renderSymbol2 '4' rgba
+    --renderSymbol 'W' fontdatatype
 
-    font2 <- makeFontFile rgba "avr/CDU.c"
-    renderSymbol '/' font2
+    saveFont "SmallCDU" font
+    renderSymbol '0' fontData
+    renderSymbol '4' fontData
+    renderSymbol 'V' fontData
